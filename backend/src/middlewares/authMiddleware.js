71a3f5f2a767d9d263e28cbef3339bdcaba2ae2a
Aminkay95 <mohamedamino882@gmail.com>
2025-12-14 00:09:28 +0300

    authMiddleware done

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/indexModel.js"
+
+
+function authMiddleware(req, res, next){
+    //Extract Token
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if(!token){
+        return res.status(401).json({message: "Unauthorized"})
+    }
+
+    //Check Token Validity
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(401).json({message: "Invalid token"})
+        }
+
+        req.user = decoded
+        next()
+    })
+
+}
+
+
+function isAdmin(req, res, next) {
+    if(req.user && req.user.role === "admin"){
+        next()
+    } else {
+        res.status(403).json({message: "Access Denied."})
+    }
+}
+
+
+export {
+    authMiddleware,
+    isAdmin
+}
